936aa3ea3195aa839053f75882522380d513a46a
christelle01 <christellecarmela@gmail.com>
2024-03-05 07:52:52 +0000

    initial commit

---
+++ 3.Les-bases-de-React/src/App.jsx
@@ -0,0 +1,13 @@
+import Container from "./lessons/1.Le-JSX/Container"
+
+function App() {
+
+  return (
+    <div className="main-content">
+      <h1>App composant racine</h1>
+      <Container />
+    </div>
+  )
+}
+
+export default App
---
+++ 3.Les-bases-de-React/src/lessons/1.Le-JSX/Container.jsx
@@ -0,0 +1,6 @@
+function Container(){
+    return(
+        <p>Hello world from container</p>
+    )
+}
+export default Container
